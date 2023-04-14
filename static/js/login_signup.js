e702cbc1a41ecf8ad39a0a0dcdb37731b7cce956
Adithya-Sr <97578285+Adithya-Sr@users.noreply.github.com>
2023-04-14 19:47:42 +0530

    fixed styles

--- static/js/login_signup.js
+++ static/js/login_signup.js
@@ -11,13 +11,15 @@ let buttonText = document.querySelector('.tick');
 
 const tickMark = "<svg width=\"45\" height=\"47\" viewBox=\"0 0 58 45\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#fff\" fill-rule=\"nonzero\" d=\"M19.11 44.64L.27 25.81l5.66-5.66 13.18 13.18L52.07.38l5.65 5.65\"/></svg>";
 buttonText.innerHTML = "Submit";
-
+ 
 button.addEventListener('click', function () {
 
     if (buttonText.innerHTML !== "Submit") {
         buttonText.innerHTML = "Submit";
+       buttonText.style.backgroundColor="#a5d8ff";
     } else if (buttonText.innerHTML === "Submit") {
         buttonText.innerHTML = tickMark;
+          buttonText.style.backgroundColor="mediumseagreen";
     }
     this.classList.toggle('button__circle');
 });
