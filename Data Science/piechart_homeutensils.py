9d99e7811e638572fed9a837393f183cb3f30034
Adhithiyaram13 <94916627+Adhithiyaram13@users.noreply.github.com>
2023-04-14 15:15:04 +0530

    Create piechart_homeutensils.py

---
+++ Data Science/piechart_homeutensils.py
@@ -0,0 +1,35 @@
+#import modules
+import matplotlib.pyplot as plt
+
+
+def main():
+    # Make an example pie plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    categories = ['Air Conditioners', 'Televisions', 'Washing Machines','Refrigerators']
+    data = [10, 12, 13, 5]
+    wedges, plt_labels = ax.pie(data, labels=categories)
+    ax.axis('equal')
+
+    make_picker(fig, wedges)
+    plt.show()
+
+
+def make_picker(fig, wedges):
+    import webbrowser
+
+    def on_pick(event):
+        wedge = event.artist
+        label = wedge.get_label()
+        webbrowser.open('http://images.google.com/images?q={0}'.format(label))
+
+    # Make wedges selectable
+    for wedge in wedges:
+        wedge.set_picker(True)
+
+    fig.canvas.mpl_connect('pick_event', on_pick)
+
+
+if __name__ == '__main__':
+    main()
